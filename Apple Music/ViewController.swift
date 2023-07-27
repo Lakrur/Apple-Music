fa0dd9d9d434bc07f9514184221c83c0b1a82615
Yehor Krupiei <yehorkrupiei@gmail.com>
2023-07-27 14:11:01 +0300

    Initial Commit

---
+++ Apple Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Music
+//
+//  Created by Yehor Krupiei on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
