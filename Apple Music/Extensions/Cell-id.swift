f3910b1931a88154a7effcab9a0cd96324268c41
Yehor Krupiei <yehorkrupiei@gmail.com>
2023-07-30 16:15:16 +0300

    realization searchbar

---
+++ Apple Music/Extensions/Cell-id.swift
@@ -0,0 +1,18 @@
+//
+////  Cell-id.swift
+////  Apple Music
+////
+////  Created by Yehor Krupiei on 27.07.2023.
+////
+//
+//import Foundation
+//
+//protocol HasCellID {
+//    static var reusableIdentifier: String { get }
+//}
+//
+//extension HasCellID {
+//    static var reusableIdentifier: String {
+//        String(describing: self)
+//    }
+//}
--- Apple Music/SearchViewController.swift
+++ Apple Music/SearchViewController.swift
@@ -7,11 +7,54 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let songs = [TrackModel(trackName: "Sky", artistName: "Playboi Carti"),
+                 TrackModel(trackName: "Place", artistName: "Playboi Carti")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setupSearchBar()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let track = songs[indexPath.row]
+        
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = UIImage(imageLiteralResourceName: "wlr")
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
